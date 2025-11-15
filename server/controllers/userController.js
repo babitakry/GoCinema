6ed6794b432ce6ab1179e6bf33cc99c2ff48f4e3
Babita Kumari <babitakumari.ky3007@gmail.com>
2025-11-15 23:23:59 +0530

    feat: added updateFavorite getFavorites and its end points:

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,4 @@
+import { clerkClient } from "@clerk/express";
 import Booking from "../models/Booking";
 
 
@@ -8,13 +9,48 @@ export const getUserBookings = async (req, res) => {
 
         const bookings = await Booking.find({ user }).populate({
             path: "show",
-            populate: {path: "movie"}
-        }).sort({createdAt: -1})
+            populate: { path: "movie" }
+        }).sort({ createdAt: -1 })
 
         res.json({
             success: true,
             bookings
         })
+    } catch (error) {
+        console.error(error.message);
+        res.json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+// API Controller Function to Add Favorite Movie in Clerk User Metadata
+export const updateFavorite = async (req, res) => {
+    try {
+        const { movieId } = req.body;
+        const userId = req.auth().userId;
+
+        const user = await clerkClient.users.getUser(userId);
+        console.log("Clerk user", user);
+
+        if(!user.privateMetadata.favorites){
+            user.privateMetadata.favorites = [];
+        }
+        if(!user.privateMetadata.favorites.includes(movieId)){
+            user.privateMetadata.favorites.push(movieId);
+        }
+        else{
+            user.privateMetadata.favorites = user.privateMetadata.favorites.filter(item => item !== movieId)
+        }
+
+        await clerkClient.users.updateUserMetadata(userId, {privateMetadata: user.privateMetadata});
+
+        res.json({
+            success: true,
+            message: "Favorite added Successfully."
+        })
+
     } catch (error) {
         console.error(error.message);
         res.json({
